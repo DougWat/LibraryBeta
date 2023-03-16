5bc24059458463b074c07e10f44b1a001ee610d7
Doug <DougWatro@gmail.com>
2023-03-16 01:36:29 -0400

    Init

---
+++ .eleventy.js
@@ -0,0 +1,14 @@
+module.exports = function(eleventyConfig){
+  eleventyConfig.addWatchTarget("./src/styles/styles.css");
+  eleventyConfig.addWatchTarget("./src/styles/partials/**.css");
+  eleventyConfig.addWatchTarget("./dist/assets/styles/styles.css");
+  eleventyConfig.addPassthroughCopy("src/assets");
+  return{
+    dir: {
+      input: "src",
+      output: "dist",
+      includes:"_includes",
+      layouts:"_layout"
+    }
+  }
+};
